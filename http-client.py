64dc5f4b268b9f55d134b1625fa644094bb96884
joshadambell <joshadambell@me.com>
2014-02-24 21:18:28 -0700

    Reordered for hardware collecting data

--- http-client.py
+++ http-client.py
@@ -3,28 +3,44 @@ import json
 import random
 import threading
 import time
+import RPi.GPIO as GPIO
+import strftime
 
 DEVICE_ID = 1
 URL = 'http://10.0.1.5:3000/mattress_data'
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(7, GPIO.IN)
+peoplecount = 0
+
+while(1):
+        presence = GPIO.input(7)
+        if(presence):
+            threading.Timer(10, sendData).start()
+            peoplecount += 1
+            time.sleep(1.5)
+        time.sleep(1)
 
 
 def sendData():
-	count = random.randrange(1, 30)
+	
 	print "send"
+
 	values = dict(mattress_datum=(dict(device_id=DEVICE_ID, 
     					 		   type='how many', 
-    					           data=count, 
+    					           data=peoplecount, 
     					           units='count', 
-    						       date='date')))
+    						       date=strftime("%Y-%m-%d %H:%M:%S"))))
 
-	req = urllib2.Request(URL)
-	req.add_header('Content-Type', 'application/json')
+	try:
+		req = urllib2.Request(URL)
+		req.add_header('Content-Type', 'application/json')
 
-	rsp = urllib2.urlopen(req, json.dumps(values))
-	content = rsp.read()
-	print rsp.getcode()
+		rsp = urllib2.urlopen(req, json.dumps(values))
+		content = rsp.read()
+		print rsp.getcode()
+	except:
+		print "Error in sending data"
 
-	threading.Timer(10, sendData).start()
 
-# start calling f now and every 60 sec thereafter
+
 sendData()
